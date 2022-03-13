9d898def4d029b1cf5efda1a4fe651caf68e60d9
Owsama Hajjar <hajjar.owsama@outlook.com>
2022-03-13 12:14:20 +0000

    adding correct choice to console log

--- assets/js/fizzbuzz.js
+++ assets/js/fizzbuzz.js
@@ -7,7 +7,7 @@ function FizzBuzz() {
     let choices = ['Pass', 'Fizz', 'Buzz', 'FizzBuzz'];
     let player_guess = 0;
     let correct_choice = 0;
-   
+
     // start and setup functions
     this.start = function () {
         number = 1;
@@ -16,12 +16,12 @@ function FizzBuzz() {
         this.updateDisplay();
         this.updateConditions();
     }
-    
+
     this.updateDisplay = function () {
         document.getElementById("number_display").innerHTML = "What about: " + number;
         document.getElementById("score_display").innerHTML = "your score is: " + score;
     }
-    
+
     this.updateConditions = function () {
         is_Fizz = (number % fizz_number == 0) || false
         is_Buzz = (number % buzz_number == 0) || false
@@ -31,9 +31,8 @@ function FizzBuzz() {
         if (is_Fizz) correct_choice = 1;
         if (is_Buzz) correct_choice = 2;
         if (is_FizzBuzz) correct_choice = 3;
-        
     }
-    
+
     this.checkIfRunning = function () {
         if (!running) {
             document.getElementById("score_display").innerHTML = "Please click on start game button first";
@@ -100,9 +99,14 @@ function FizzBuzz() {
     }
 
     this.previousNumber = function () {
+        console.log('Correct Choice was', choices[correct_choice]);
         if (!is_Fizz && !is_Buzz) {
             document.getElementById("previous_number_display")
                 .innerHTML = number + " was <b>not divisible by either " + fizz_number + " or " + buzz_number + "</b>";
+        }
+        if (is_FizzBuzz) {
+            document.getElementById("previous_number_display")
+                .innerHTML = number + " was <b>divisible by " + fizz_number + " and " + buzz_number + "</b>";
         }
         if (is_Fizz) {
             document.getElementById("previous_number_display")
@@ -111,10 +115,6 @@ function FizzBuzz() {
         if (is_Buzz) {
             document.getElementById("previous_number_display")
                 .innerHTML = number + " was <b>divisible by " + buzz_number + "</b>";
-        }
-        if (is_FizzBuzz) {
-            document.getElementById("previous_number_display")
-                .innerHTML = number + " was <b>divisible by " + fizz_number + " and " + buzz_number + "</b>";
         }
     }
 
@@ -127,4 +127,4 @@ function FizzBuzz() {
 
 }
 
-const game = new Fizzbuzz;
+const game = new FizzBuzz;
